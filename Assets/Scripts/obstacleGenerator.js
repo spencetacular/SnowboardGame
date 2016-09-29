5e4e8b04e9031e357d0324e534bc88ed8c025ce4
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-09-29 11:25:57 -0600

    obstacle generator working

---
+++ Assets/Scripts/obstacleGenerator.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+public class obstacleGenerator extends MonoBehaviour {
+	public var obstacleDesity = 10;
+	public var obstacle1: GameObject;
+	public var obstacle1Density = 1.0;
+
+	function Start() {
+		var numObstacles = 	obstacleDesity * obstacle1Density;
+		Debug.Log(numObstacles);
+		for (var i: int = 0; i < numObstacles; i++) {
+			var y = Random.Range(5.0,-10.0);
+			var x = Random.Range(7.0,-7.0);
+			var tree = Instantiate(obstacle1, new Vector3(x, y, 0), Quaternion.identity);
+//			tree.tag = "obstacle";
+			}
+	}
+}
