c51bdd16c1d539dd238f08428868eb77e3083402
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-09-29 14:59:10 -0600

    working on player control

--- Assets/Scripts/obstacleGenerator.js
+++ Assets/Scripts/obstacleGenerator.js
@@ -1,5 +1,6 @@
 ﻿#pragma strict
 public class obstacleGenerator extends MonoBehaviour {
+	public var gameSpeed = 1.0;
 	public var obstacleDesity = 10;
 	public var obstacle1: GameObject;
 	public var obstacle1Density = 1.0;
@@ -11,7 +12,11 @@ public class obstacleGenerator extends MonoBehaviour {
 			var y = Random.Range(5.0,-10.0);
 			var x = Random.Range(7.0,-7.0);
 			var tree = Instantiate(obstacle1, new Vector3(x, y, 0), Quaternion.identity);
-//			tree.tag = "obstacle";
+			tree.transform.parent = this.transform;
 			}
+	}
+
+	function Update () {
+		transform.Translate(0, Time.deltaTime * gameSpeed, Time.deltaTime);
 	}
 }
---
+++ Assets/Scripts/playerMovement.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+public class playerMovement extends MonoBehaviour {
+	public var obstacles: GameObject;
+	public var movementSpeed = 0.5;
+
+	function Start () {
+
+	}
+
+	function Update () {
+		if (Input.GetKeyDown ("left")){
+			transform.Translate(movementSpeed * -1.0, 0, 0);
+		}
+
+		if (Input.GetKeyDown ("right")){
+			transform.Translate(movementSpeed, 0, 0);
+		}
+
+		if (Input.GetKeyDown ("down")){
+			
+		}
+
+	}
+}
