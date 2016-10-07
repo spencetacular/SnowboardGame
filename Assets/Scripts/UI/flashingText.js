ad5c73bd49b07ae83724fb112b6f63995ae4a727
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-10-07 09:30:22 -0600

    sick trick and snowboarder animations added

---
+++ Assets/Scripts/UI/flashingText.js
@@ -0,0 +1,38 @@
+﻿#pragma strict
+
+var textObject : UnityEngine.UI.Text;
+public var secondsBetweenFlash  = 1.0;
+var flashTicker  = 0.0;
+var visible = true;
+
+
+function Start () {
+	flashTicker = secondsBetweenFlash;
+	textObject = GetComponent(UnityEngine.UI.Text);
+}
+
+function Update () {
+
+	if (flashTicker >= 0) {
+		flashTicker -= Time.deltaTime;
+	} else {
+		visible = !visible;
+		flashTicker = secondsBetweenFlash;
+	}
+
+	if (visible) {
+		textObject.enabled = true;
+	} else {
+		textObject.enabled = false;
+	}
+
+	
+	// Debug.Log(Time.timeSinceLevelLoad);
+	// Debug.Log(Mathf.FloorToInt(Time.timeSinceLevelLoad));
+
+	// if (Mathf.FloorToInt(Time.timeSinceLevelLoad) % 2 == 0) {
+	// 	textObject.enabled = false;
+	// } else {
+	// 	textObject.enabled = true;
+	// }
+}
---
+++ Assets/Scripts/UI/popUps.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+var image : UnityEngine.UI.Image;
+public var secondsBetweenFlash  = 1.0;
+var flashTicker  = 0.0;
+var visible = true;
+
+
+
+
+function Start () {
+	flashTicker = secondsBetweenFlash;
+	// textObject = GetComponent(UnityEngine.UI.Text);
+	image = GetComponent(UnityEngine.UI.Image);
+}
+
+function Flash () {
+
+	if (flashTicker >= 0) {
+		flashTicker -= Time.deltaTime;
+	} else {
+		visible = !visible;
+		flashTicker = secondsBetweenFlash;
+	}
+
+	if (visible) {
+		image.enabled = true;
+	} else {
+		image.enabled = false;
+	}
+}
+
+function Update () {
+
+
+	Flash ();
+	// Debug.Log(Time.timeSinceLevelLoad);
+	// Debug.Log(Mathf.FloorToInt(Time.timeSinceLevelLoad));
+
+	// if (Mathf.FloorToInt(Time.timeSinceLevelLoad) % 2 == 0) {
+	// 	textObject.enabled = false;
+	// } else {
+	// 	textObject.enabled = true;
+	// }
+}
