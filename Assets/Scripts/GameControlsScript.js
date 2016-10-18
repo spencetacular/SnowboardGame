55d79d8d92a23820e0c48975504e4d871fd74c17
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-10-18 11:46:21 -0600

    formatting game over canvas

--- Assets/Scripts/GameControlsScript.js
+++ Assets/Scripts/GameControlsScript.js
@@ -15,9 +15,9 @@ function GameOver (time : int, score : int) {
 function Update () {
 	var secondsElapsed = Mathf.FloorToInt(Time.timeSinceLevelLoad);
 
-	if (gameLength - secondsElapsed <= 0) {
+	if (gameLength - secondsElapsed <= 0 && gameOver == false) {
 		gameOver = true;
-		var score = GameObject.Find("Score").GetComponent(scoreScript).score;
+		var score = GameObject.Find("score").GetComponent(scoreScript).score;
 		GameObject.Find("gameOverCanvas").GetComponent(GameOverScript).GameOver(score);
 
 	}
