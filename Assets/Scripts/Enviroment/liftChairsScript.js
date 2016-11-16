f8e5fe193762d8cdff62dd4410986d2b78b109a3
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-11-16 11:50:19 -0700

    chairs working

--- Assets/Scripts/Enviroment/liftChairsScript.js
+++ Assets/Scripts/Enviroment/liftChairsScript.js
@@ -1,20 +1,19 @@
 ﻿#pragma strict
 
 var chairs : GameObject[];
-//var chair2 : GameObject;
-//var chair3 : GameObject;
-var chairEmpty : GameObject;
 public var chairSpeed = 0.5;
 private var chairStartPos = new Vector3();
-private var chairEmptyStartPos  = new Vector3();
 var playerMovement : playerMovementScript;
+public var empty : boolean;
+private var  direction : float;
+private var screenHeight = 5.0;
 
 
 function Start () {
-//	chairStartPos  = chairs[0].transform.position.y;
 	chairStartPos = chairs[chairs.length -1].transform.position;
-	chairEmptyStartPos = chairEmpty.transform.position;
 	playerMovement = GameObject.Find("player").GetComponent(playerMovementScript);
+	direction = empty ? -1 : 1;
+		
 }
 
 function ChairsMovement() {
@@ -22,44 +21,19 @@ function ChairsMovement() {
 		for (c in chairs){
 
 			if ( playerMovement.downhill ) 
-				c.transform.Translate(0, Time.deltaTime * playerMovement.gameSpeed + Time.deltaTime  * chairSpeed, 0);
+				c.transform.Translate(0, Time.deltaTime * playerMovement.gameSpeed + (Time.deltaTime  * chairSpeed * direction), 0);
 			else
-				c.transform.Translate(0, Time.deltaTime  * chairSpeed, 0);
+				c.transform.Translate(0, Time.deltaTime  * chairSpeed * direction, 0);
 
-			if (c.transform.position.y >= 5.0) 
-			c.transform.position = chairStartPos;
-			
 
+			if (c.transform.position.y >= screenHeight) 
+				c.transform.position = chairStartPos;
 		}
-
 
 	}
 
-function ChairEmptyMovement () {
-	if ( playerMovement.downhill ) 
-		chairEmpty.transform.Translate(0, Time.deltaTime * playerMovement.gameSpeed - Time.deltaTime  * chairSpeed, 0);
-	else
-		chairEmpty.transform.Translate(0, -1 * Time.deltaTime  * chairSpeed, 0);
-
-	if (chairEmpty.transform.position.y <= -5.0) 
-		chairEmpty.transform.position = chairEmptyStartPos;
-	
-
-}
 
 function Update () {
 	ChairsMovement();
-	ChairEmptyMovement();
-
-//	for (var i = 0, i < chairs.count, i++)
-
-//	for (c in chairs) {
-//
-//		if (c.transform.position.y >= 5.0) {
-//			c.transform.position = chairStartPos;
-//			
-//		}
-////		transform.Translate(0, Time.deltaTime * chairSpeed, 0);
-//	}
 
 }
