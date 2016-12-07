a237336cafe67a8fff63b05510305ee030f786c4
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-12-07 13:20:05 -0700

    working on coin

---
+++ Assets/Scripts/Obstacles/coinScript.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public var goldBonus = 500;
+public var magentaBonus = 1000;
+
+public var score : scoreScript;
+
+function Bonus () {
+
+	if (this.tag == "coinGold") {
+		score.score += goldBonus;
+	}
+
+	if (this.tag == "coinMagenta") {
+		score.score += magentaBonus;
+	}
+}
