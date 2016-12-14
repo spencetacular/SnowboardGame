7542e730fce5c04bb3986a3187a75590b082a1a7
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2016-12-13 17:14:19 -0700

    new analytics bar

---
+++ Assets/Scripts/Options/analyticsButtonScript.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+var anaBar : GameObject;
+var anaText : Text;
+
+
+function Start () {
+
+}
+
+function Show () {
+	anaBar.SetActive(true);
+
+}
+
+
+function Hide () {
+//	anaBar.enabled = false;
+}
+
+function Delete () {
+	PlayerPrefs.DeleteKey("Log");
+	anaText.text = "";
+}
+function Update () {
+
+}
