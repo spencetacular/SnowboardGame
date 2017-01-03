60a6c3464e6639f4731a39cf9f516ae9ed38de77
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2017-01-03 11:47:53 -0700

    adding finger gestures UI

---
+++ Assets/Scripts/UI/Fingers/fingersScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var myDevice : device;
+
+function Start () {
+	myDevice = new device();
+
+	if ( myDevice.mobile ) {
+		this.enabled = true;
+	} else
+		this.enabled = false;
+}
+
+function Update () {
+
+}
