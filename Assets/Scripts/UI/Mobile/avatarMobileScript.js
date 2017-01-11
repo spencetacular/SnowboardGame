3ee94aadc597b3c57c344b6b5b775d5b710557d5
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2017-01-11 10:52:33 -0700

    buttons press and rotate for options screen

--- Assets/Scripts/UI/Mobile/avatarMobileScript.js
+++ Assets/Scripts/UI/Mobile/avatarMobileScript.js
@@ -10,12 +10,8 @@ function Start () {
 
 	if (myDevice.mobile && !myDevice.iPad) {
 		GetComponent(UnityEngine.UI.Image).transform.localScale = new Vector3(scale, scale, scale);
-//		logoSmall.transform.localScale = new Vector3(1.0, 1.0, 1.0);
 			if (logoSmall) { 
-				Debug.Log("Small");
-//				logoSmall.localScale = new Vector3(0.8, 0.8, 0.8);
 				logoSmall.localPosition = new Vector3(0.0, 160.0, 0.0);
-//				logoSmall.position = new Vector3(0.0, 100, 0.0);
 			}
 	}
 }
---
+++ Assets/Scripts/UI/Mobile/buttonsScript.js
@@ -0,0 +1,69 @@
+﻿#pragma strict
+
+import UnityEngine.UI;
+
+public var swipe : swipeScript;
+public var arrowUp : Sprite;
+public var arrowDown : Sprite;
+public var buttonUp : Sprite;
+public var buttonDown : Sprite;
+
+public var arrowRight : Image;
+public var arrowLeft : Image;
+public var button : Image;
+public var optionsScreen = false;
+var downTime = 0.20;
+
+
+
+
+function Start () {
+
+	arrowRight.sprite = arrowUp;
+	arrowLeft.sprite = arrowUp;
+	button.sprite = buttonUp;
+
+	if (optionsScreen) {
+		arrowRight.GetComponent(RectTransform).localRotation = new  Quaternion.Euler(0.0, 0.0, 90.0);
+		arrowLeft.GetComponent(RectTransform).localRotation = new  Quaternion.Euler(0.0, 0.0, -90.0);
+	}
+}
+
+function PressRight () {
+	arrowRight.sprite = arrowDown;
+	Invoke("ArrowUp",downTime);
+}
+
+function PressLeft () {
+	arrowLeft.sprite = arrowDown;
+	Invoke("ArrowUp",downTime);
+}
+
+function PressButton () {
+	button.sprite = buttonDown;
+	Invoke("ButtonUp",downTime);
+}
+
+function ArrowUp () {
+	arrowRight.sprite = arrowUp;
+	arrowLeft.sprite = arrowUp;
+}
+
+function ButtonUp () {
+	button.sprite = buttonUp;
+}
+
+function Update () {
+
+	if (swipe.Swipe() == "right") {
+		PressRight ();
+	}
+	if (swipe.Swipe() == "left") {
+		PressLeft ();
+	}
+	if (swipe.Swipe() == "enter") {
+		PressButton ();
+	}
+		
+
+}
