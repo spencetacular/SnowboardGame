2aab2421bc439b25b9e163e4b0e8bed5b3049ae0
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2017-01-11 09:47:48 -0700

    scaling up istructions for mobile

--- Assets/Scripts/UI/Mobile/avatarMobileScript.js
+++ Assets/Scripts/UI/Mobile/avatarMobileScript.js
@@ -1,13 +1,23 @@
 ﻿#pragma strict
+import UnityEngine.UI;
 
 var myDevice : device;
-
+public var scale = 1.5;
+var logoSmall : RectTransform;
 
 function Start () {
 	myDevice = new device();
 
-	if (myDevice.mobile && !myDevice.iPad)
-		GetComponent(UnityEngine.UI.Image).transform.localScale = new Vector3(1.5, 1.5, 1.5);
+	if (myDevice.mobile && !myDevice.iPad) {
+		GetComponent(UnityEngine.UI.Image).transform.localScale = new Vector3(scale, scale, scale);
+//		logoSmall.transform.localScale = new Vector3(1.0, 1.0, 1.0);
+			if (logoSmall) { 
+				Debug.Log("Small");
+//				logoSmall.localScale = new Vector3(0.8, 0.8, 0.8);
+				logoSmall.localPosition = new Vector3(0.0, 160.0, 0.0);
+//				logoSmall.position = new Vector3(0.0, 100, 0.0);
+			}
+	}
 }
 
 function Update () {
