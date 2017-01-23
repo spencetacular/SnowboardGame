afe37810a7f662f5ae994d348785a420d0fe3ca9
Spencer Christopherson <spencer.christopherson@wearetopsecret.com>
2017-01-23 13:27:22 -0700

    fixed joystick popup bug

--- Assets/Scripts/UI/joystickScript.js
+++ Assets/Scripts/UI/joystickScript.js
@@ -6,30 +6,27 @@ import UnityEngine.UI;
 public var joystickImage : Image;
 public var joystickAnim : Animator;
 public var mobileBox : GameObject;
-//public var mobileFinger : Image;
-public var secondsBetweenFlash  = 1.0;
-
+public var playerLives : playerLivesScript;
 var myDevice : device;
 
 function Start () {
 	myDevice = new device();
 
-//	joystickImage = GetComponent(Image);
-//	joystickAnim = GetComponent(Animator);
 	joystickImage.enabled = false;
 	mobileBox.SetActive(false);
-//	mobileBox.enabled = false;
-//	mobileFinger.enabled = false;
+
 }
 
 function PopOn () {	
-	if (!myDevice.mobile) {
-		joystickImage.enabled = true;
-		joystickAnim.SetTrigger("wrecked");
-	} else {
-		mobileBox.SetActive(true);
-//		mobileFinger.enabled = true;
-//		FingerFlash();
+	Debug.Log(playerLives.livesLeft);
+	if (playerLives.livesLeft > 1) {
+		if (!myDevice.mobile) {
+			joystickImage.enabled = true;
+			joystickAnim.SetTrigger("wrecked");
+		} else {
+			mobileBox.SetActive(true);
+
+		}
 	}
 
 }
@@ -39,18 +36,9 @@ function PopOff () {
 		joystickImage.enabled = false;
 	} else {
 		mobileBox.SetActive(false);
-//		mobileFinger.enabled = false;
+
 	}
 }
-
-//function FingerFlash () {
-//	mobileFinger.enabled = true;
-//	Invoke ("FingerOff", secondsBetweenFlash/2.0);
-//}
-//function FingerOff () {
-//	mobileFinger.enabled = false;
-//	Invoke ("FingerFlash", secondsBetweenFlash/2.0);
-//}
 
 function Update () {
 
